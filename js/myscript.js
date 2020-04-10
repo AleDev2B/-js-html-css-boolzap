85156eea92952bad9e30ad172fdf498f5157d4c7
Alessandro <alecritelli@gmail.com>
2020-04-10 18:56:05 +0200

    add search user functionality

--- js/myscript.js
+++ js/myscript.js
@@ -13,9 +13,43 @@ buttonSend.click(
     // console.log(msg);
     chatWin.append('<div class="sent-msg rightAllignClass"><div class="box-msg centraQualcosaClass colorgreenclass"><p class="normalText">' + msg + '</p></div></div>');
 
-    inputMsg.val('');
-  }
-);
+    inputMsg.val("");
 
+    //dopo un secondo
+      setTimeout(
+        function(){
+          chatWin.append('<div class="sent-msg rightAllignClass"><div class="box-msg centraQualcosaClass colorgreenclass"><p class="normalText">');
+        }
+        ,1000);
+      // deve apparire un nuovo msg con un testo sempre uguale (statico)
+    }
+  );
+
+
+  // filtro contatti
+    //gestirte evento su tastiera (oppure su click di bottone di input ricerca)
+  $('#contacts-filter').keyup(
+    function () {
+
+    // salvarmi input utente in campo del filtro (stringa1)
+    var stringaFiltro = $(this).val().toLowerCase();
+
+    // selezionare tutti i blocchi di contatto e ciclare tra di essi (each())
+    $('.activeUser').each(
+      function () {
+      //salvo in una var il valore del testo del nome nel contatto (stringa2)
+      var stringaNome = $(this).find('.contact-name').text().toLowerCase();
+      // confronto per vedere se la stringa inserita nell'input è inclusa nel nome del contatto
+        //stringa2.includes(stringa1)
+        if(stringaNome.includes(stringaFiltro)){
+        //se l'occorenza è stata trovata lascio il blocco di contatto visibile
+          $(this).show();
+        } else {
+          // altrimenti lo rendo non visibile (this)
+          $(this).hide();
+        }
+
+    });
+  });
 
 });
