886b2036b2a7ad669cf50281bd76995f967348f9
Alessandro <alecritelli@gmail.com>
2020-04-10 18:01:26 +0200

    adding msg on screen pressing button
    (undefined msg error)

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,21 @@
 
+$(document).ready(function(){
+
+
+var chatWin = $('.main-partedx');
+var inputMsg = $('.new-message-inputs');
+var buttonSend = $('.buttonSend');
+
+//gestisco evento su bottone di invio
+buttonSend.click(
+  function () {
+    var msg = inputMsg.val();
+    // console.log(msg);
+    chatWin.append('<div class="sent-msg rightAllignClass"><div class="box-msg centraQualcosaClass colorgreenclass"><p class="normalText">' + msg + '</p></div></div>');
+
+    inputMsg.val('');
+  }
+);
+
+
+});
