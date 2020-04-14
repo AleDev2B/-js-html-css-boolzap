6446cddcd4eb4dcedbe9839f4381b60adc825534
Alessandro <alecritelli@gmail.com>
2020-04-14 15:26:09 +0200

    fix undef. msg

--- js/myscript.js
+++ js/myscript.js
@@ -13,11 +13,11 @@ var buttonSend = $('.buttonSend');
 //gestisco evento su bottone di invio
 buttonSend.click(
   function () {
-    var msg = inputMsg.val('');
+    var msg = inputMsg.val();
     // console.log(msg);
     chatWin.append('<div class="sent-msg rightAllignClass"><div class="box-msg centraQualcosaClass colorgreenclass"><p class="normalText">' + msg + '</p></div></div>');
 
-    inputMsg.val('');
+    inputMsg.val();
 
     //dopo un secondo
       setTimeout(
